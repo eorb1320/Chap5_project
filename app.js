4dea511ca8a15fdc608b98e1366d6ed3031e835d
doremilan <yooossi@gmail.com>
2022-04-09 21:03:19 +0900

    수정끝
    수정끝(이길)

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use((req, res) => {
-  res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // 특정 도메인 허용
-});
+const app = express();
+const port = 5000;
 
 mongoose.connect("mongodb://localhost:27017/team7-db", {
   useNewUrlParser: true,
@@ -15,13 +13,6 @@ mongoose.connect("mongodb://localhost:27017/team7-db", {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
-const app = express();
-const port = 5000;
-
-// const corsOption = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-// };
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.get("/cors-test", (req, res) => {
