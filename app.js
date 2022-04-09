7d881bec1487616d4dce0f01fc15928319cd943a
doremilan <yooossi@gmail.com>
2022-04-09 17:51:46 +0900

    수정
    수정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect("mongodb://localhost:27017/team7-db", {
   useNewUrlParser: true,
@@ -11,6 +12,11 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.get("/cors-test", (req, res) => {
+  res.send("hi");
+});
 
 const commentsRouter = require("./routes/comment");
 
