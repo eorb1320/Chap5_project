c466d94a34bdb93340f4a7fae7fa8111739d52bb
doremilan <yooossi@gmail.com>
2022-04-09 19:03:36 +0900

    port 수정
    port 수정

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(cors());
 app.get("/cors-test", (req, res) => {
