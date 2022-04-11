c38106fc06424193ef58daf4e943b3e139a71b1c
doremilan <yooossi@gmail.com>
2022-04-11 21:44:27 +0900

    수정
    수정

--- routes/comment.js
+++ routes/comment.js
@@ -16,6 +16,8 @@ const router = express.Router();
 router.post("/comments", async (req, res) => {
   const { comment, createdAt } = req.body;
   const postId = req.params;
+  console.log(postId);
+  console.log(typeof postId);
   // const userId = res.locals.user._id;
   // const { nickname, userImage } = res.locals.user;
   // console.log(userId);
