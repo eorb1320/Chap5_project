5ac4abb9f277dd67675e08ad40a892145ebe7004
doremilan <yooossi@gmail.com>
2022-04-11 21:09:47 +0900

    수정테스트
    수정테스트

--- schemas/post.js
+++ schemas/post.js
@@ -1,41 +1,41 @@
-const mongoose = require("mongoose");
-//mongodb 데어터 타입 지정
-const postsSchema = new mongoose.Schema({
-     emaill: {
-         type: String,
-         required: true,
-     },
-    title: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    item: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-     profile: {
-         type: String,
-         required: true,
-     },
-    createdAt: {
-        type: Date,
-    },
+// const mongoose = require("mongoose");
+// //mongodb 데어터 타입 지정
+// const postsSchema = new mongoose.Schema({
+//      emaill: {
+//          type: String,
+//          required: true,
+//      },
+//     title: {
+//         type: String,
+//         required: true,
+//     },
+//     content: {
+//         type: String,
+//         required: true,
+//     },
+//     item: {
+//         type: String,
+//         required: true,
+//     },
+//     image: {
+//         type: String,
+//         required: true,
+//     },
+//      profile: {
+//          type: String,
+//          required: true,
+//      },
+//     createdAt: {
+//         type: Date,
+//     },
 
-});
-//기존에 저장되는 id값을 postsId로이용
-postsSchema.virtual("postId").get(function () {
-    return this._id.toHexString();
-});
-postsSchema.set("toJSON", {
-    virtuals: true,
-});
+// });
+// //기존에 저장되는 id값을 postsId로이용
+// postsSchema.virtual("postId").get(function () {
+//     return this._id.toHexString();
+// });
+// postsSchema.set("toJSON", {
+//     virtuals: true,
+// });
 
-module.exports = mongoose.model("Posts", postsSchema);
+// module.exports = mongoose.model("Posts", postsSchema);
