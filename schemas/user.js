b5ae1c36e9ab416c35f891bce1721eac9c46eed4
doremilan <yooossi@gmail.com>
2022-04-12 13:05:27 +0900

    수정
    수정

--- schemas/user.js
+++ schemas/user.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    email: {
-        type:String,
-        required: true, // null이 들어갈수 없음 
-        unique:true,
-    },
-    nickname : {
-       type:String,
-    },
-    password : {
-        type:String,
-     },
-     profile : {
-        type:String,
-     }
-});
+// const UserSchema = new mongoose.Schema({
+//     email: {
+//         type:String,
+//         required: true, // null이 들어갈수 없음
+//         unique:true,
+//     },
+//     nickname : {
+//        type:String,
+//     },
+//     password : {
+//         type:String,
+//      },
+//      profile : {
+//         type:String,
+//      }
+// });
 
-UserSchema.virtual("_Id").get(function () {
-    return this._id.toHexString(); // 가상의 아이디를 만들어서 
-    });
-    
-    UserSchema.set("toJSON", {
-    virtuals: true,
-    });
+// UserSchema.virtual("_Id").get(function () {
+//     return this._id.toHexString(); // 가상의 아이디를 만들어서
+//     });
 
- module.exports = mongoose.model("User", UserSchema);
+//     UserSchema.set("toJSON", {
+//     virtuals: true,
+//     });
+
+//  module.exports = mongoose.model("User", UserSchema);
